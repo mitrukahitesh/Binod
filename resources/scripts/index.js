a1a5e704dc549c4eb1338085f74f2cf8f6016337
mitrukahitesh <mitrukahitesh@gmail.com>
2021-04-24 12:13:50 +0530

    frontend completed

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -4,6 +4,10 @@ const banner = slider[0].getElementsByClassName("banner")
 const nextBannerButton = document.getElementById("nextBanner")
 var setBanner = 0;
 var bannerInterval
+const sendEmail = document.getElementById("sendEmail")
+const customerName = document.getElementById("name")
+const email = document.getElementById("email")
+const query = document.getElementById("query")
 
 function bannerChanger() {
     for (var i = 0; i < banner.length; ++i) {
@@ -29,4 +33,14 @@ bannerChanger()
 var scrollingElement = (document.scrollingElement || document.body);
 contactUs.onclick = function () {
     scrollingElement.scrollTop = scrollingElement.scrollHeight - scrollingElement.clientHeight
+}
+
+sendEmail.onclick = function () {
+    var receiverName = customerName.value
+    var receiver = email.value
+    var message = query.value
+    var URL = `https://queu-e.herokuapp.com/?resource=verify&function=sendOtp&email=${receiver}`
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", URL)
+    xhr.send()
 }
